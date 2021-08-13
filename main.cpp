a673a5321c09c7d0f642ee71a791844e447c1941
Alex Liu <alex.liuyining@outlook.com>
2021-08-12 23:09:21 -0400

    Augmented quiz difficulty by limiting number of candidate choices.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 // This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
+#include <algorithm>
+#include <random>
 #include "VocabDictionary.h"
 #include "RequesterSocket.h"
+
+const int numQuizCandidates = 5;
 
 //void startPythonServer()
 //{
@@ -12,15 +16,20 @@
 //    system(command.c_str());
 //}
 
-void quizRandomWord(VocabDictionary * myDictionary)
+void quizRandomWord(VocabDictionary * myDictionary, int & mistakes)
 {
     static std::vector <std::string> quizzedWords;
     static std::vector <std::string> allDefinitions;
+    std::vector <std::string> candidateWords;
+    std::vector <std::string> candidateDefinitions;
     std::string selectedRandomWord;
+    std::string possibleCandidate;
     int userInput;
     std::string answer;
+    int counter = 0;
 
     allDefinitions = myDictionary->getAllDefinitions();
+    // Randomly select the word and definition to be quizzed on
     while (true)
     {
         selectedRandomWord = myDictionary->getRandomWord();
@@ -31,19 +40,40 @@ void quizRandomWord(VocabDictionary * myDictionary)
         }
     }
     answer = myDictionary->getDef(selectedRandomWord);
+    candidateDefinitions.push_back(answer);
+
+    // Randomly select a number of candidate definitions 
+    while (counter < numQuizCandidates)
+    {
+        possibleCandidate = myDictionary->getRandomWord();
+        if (std::find(candidateWords.begin(), candidateWords.end(), possibleCandidate) == candidateWords.end() && !(possibleCandidate == selectedRandomWord))
+        {
+            candidateWords.push_back(possibleCandidate);
+            counter++;
+        }
+    }
+    for (int i=0; i<candidateWords.size(); i++)
+    {
+        candidateDefinitions.push_back(myDictionary->getDef(candidateWords[i]));
+    }
+    auto rd = std::random_device{};
+    auto rng = std::default_random_engine{ rd() };    
+    std::shuffle(std::begin(candidateDefinitions), std::end(candidateDefinitions), rng);
+
     // Show the user all the available definitions 
     std::cout << "Select definition for: " << selectedRandomWord << std::endl;
     std::cout << "Available choices: " << std::endl;
-    for (int i = 0; i < allDefinitions.size(); i++)
+    for (int i=0; i<candidateDefinitions.size(); i++)
     {
-        std::cout << (i+1) << ":" << allDefinitions[i] << std::endl;
+        std::cout << (i+1) << ": " << candidateDefinitions[i] << std::endl;
+        std::cout << std::endl;
     }
     while (true)
     {
         std::cout << "Enter answer by index ";
         std::cin >> userInput;
         userInput -= 1;
-        if (allDefinitions[userInput] == answer)
+        if (candidateDefinitions[userInput] == answer)
         {
             std::cout << "Correct!" << std::endl;
             std::cout << answer << std::endl;
@@ -51,6 +81,7 @@ void quizRandomWord(VocabDictionary * myDictionary)
         }
         else
         {
+            mistakes++;
             std::cout << "Incorrect, try again." << std::endl;
         }
     }
@@ -66,6 +97,8 @@ int main()
     int choice;
     int num_words;
     int result; 
+    int wordsQuizzed = 0;
+    int mistakes = 0;
     std::vector <std::string> pair;
     VocabDictionary myDictionary("vocabulary.txt");
 
@@ -80,6 +113,7 @@ int main()
     {
         std::cout << "Enter 1 to display vocab list, enter 2 to query word, enter 3 to add new word, enter 4 to quiz random words: ";
         std::cin >> choice;
+        std::cout << std::endl;
         if (std::cin.fail())
         {
             std::cout << "Invalid entry" << std::endl;
@@ -140,7 +174,10 @@ int main()
         }
         else if (choice == 4)
         {
-            quizRandomWord(&myDictionary);
+            quizRandomWord(&myDictionary, mistakes);
+            wordsQuizzed++;
+            std::cout << "Quizzed " << wordsQuizzed << " words, made " << mistakes << " wrong choices." << std::endl;
+            std::cout << std::endl;
         }
         else {
             std::cout << "Invalid entry" << std::endl;
